30b5371ae6549be5046c588301550f9128613571
songwon Lee <65889472+songwon0327@users.noreply.github.com>
2022-11-25 16:12:28 +0900

    load cell 무게 측정 코드
    Signed-off-by: songwon Lee <65889472+songwon0327@users.noreply.github.com>

---
+++ sketch_nov13a.ino
@@ -0,0 +1,34 @@
+#include "HX711.h"
+ 
+const int LOADCELL_DOUT_PIN = 4;
+const int LOADCELL_SCK_PIN = 5;
+ 
+HX711 scale;
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+}
+ 
+void loop() 
+{
+ 
+  if (scale.is_ready()) 
+  {
+    long reading = scale.read();
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } 
+  else 
+  {
+    Serial.println("HX711 not found.");
+  }
+ 
+  delay(1000);
+  
+}
+
+
+
+
