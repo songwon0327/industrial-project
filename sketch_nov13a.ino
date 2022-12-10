554ca9dac01c636a0e3cc1f352a32751b8583490
songwon Lee <65889472+songwon0327@users.noreply.github.com>
2022-12-10 13:42:11 +0900

    Combined Code
    Signed-off-by: songwon Lee <65889472+songwon0327@users.noreply.github.com>

--- sketch_nov13a.ino
+++ sketch_nov13a.ino
@@ -1,34 +1,89 @@
 #include "HX711.h"
- 
-const int LOADCELL_DOUT_PIN = 4;
-const int LOADCELL_SCK_PIN = 5;
+
+const int DOUT_PIN = 4;
+const int SCK_PIN = 5;
+const int PLAYE = 9;
  
 HX711 scale;
- 
+float caliFactor = -51500;
+
 void setup() 
 {
   Serial.begin(9600);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.begin(DOUT_PIN, SCK_PIN);
+  scale.set_scale();
+  scale.tare();
+  pinMode(DOUT_PIN, INPUT);
+  pinMode(PLAYE,OUTPUT);
 }
  
 void loop() 
 {
+  Serial.print("Reading: ");
+  float curWeight = scale.get_units();
+  Serial.print(curWeight, 1);
+  Serial.print(" kg"); 
+  Serial.print(" calibration_factor: ");
+  Serial.print(caliFactor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    esp(Serial.readString(), curWeight);
+    char temp = Serial.read();
  
-  if (scale.is_ready()) 
-  {
-    long reading = scale.read();
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } 
-  else 
-  {
-    Serial.println("HX711 not found.");
+    switch(temp) // 로드셀 무게 영점 조절
+    {
+      case 'A':
+        caliFactor += 10;
+        break;
+      case 'B':
+        caliFactor += 50;
+        break;
+      case 'C':
+        caliFactor += 100;
+        break;
+      case 'D':
+        caliFactor += 1000;
+        break;
+ 
+      case 'a':
+        caliFactor -= 10;
+        break;
+      case 'b':
+        caliFactor -= 50;
+        break;
+      case 'c':
+        caliFactor -= 100;
+        break;
+      case 'd':
+        caliFactor -= 1000;
+        break;
+    }
   }
- 
+
   delay(1000);
-  
 }
 
+void esp(String ch, float curWeight)
+{
+  if(ch == "0"){
+    Serial.println("TURN ON THE BLOCK");
+    speaker(curWeight);
+  }
+  if(ch == "1"){
+    Serial.println("TURN OFF THE BLOCK");
+  }
+  return;
+}
 
-
-
+void speaker(float curWeight)
+{
+  if(curWeight >= 20)
+  {
+    digitalWrite(PLAYE, HIGH); 
+    delay(5000);
+    digitalWrite(PLAYE, LOW); 
+  }
+  return;
+}
